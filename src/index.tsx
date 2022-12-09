59d6c178c5975edb4d856cc603e17a5c0edb7969
C-bv <charles.bouveret.dev@gmail.com>
2022-12-09 22:51:39 +0100

    ✨ ModalProvider in index

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ToastProvider } from './contexts/ToastContext';
+import { ModalProvider } from './contexts/ModalContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-    <ToastProvider>
-        <App />
-    </ToastProvider>
+    <ModalProvider>
+        <ToastProvider>
+            <App />
+        </ToastProvider>
+    </ModalProvider>
 );
