14726d92a932844a0388d8d7831e88329c07118a
C-bv <charles.bouveret.dev@gmail.com>
2022-12-11 21:37:06 +0100

    💄 random css

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
+import { useState } from "react";
 import Navbar from "./components/navbar/Navbar";
 import Widgets from "./components/widgets/Widgets";
-import SettingsModal from "./components/settings/SettingsModal";
+import Settings from "./components/settings/Settings";
 import './assets/scss/App.scss';
 
 const App = () => {
+    const [openSettings, setOpenSettings] = useState(false);
+
     return (
         <main>
-            <Navbar />
+            <Navbar
+                openSettings={openSettings}
+                setOpenSettings={setOpenSettings}
+            />
             <Widgets />
-            <SettingsModal />
+            {openSettings && <Settings setOpenSettings={setOpenSettings} />}
         </main>
     );
 };
