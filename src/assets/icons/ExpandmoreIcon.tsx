5b0ab5d617507e40433e449f32253feddc290d18
C-bv <charles.bouveret.dev@gmail.com>
2022-12-25 09:42:32 +0100

    💄 searchbar icons

---
+++ src/assets/icons/ExpandmoreIcon.tsx
@@ -0,0 +1,16 @@
+const ExpandmoreIcon = (props: any) => {
+    return (
+        <svg
+            width={24}
+            height={24}
+            viewBox='0 0 24 24'
+            xmlns='http://www.w3.org/2000/svg'
+            {...props}
+        >
+            <path d='M24 24H0V0h24v24z' fill='none' opacity='.87' />
+            <path d='M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6-1.41-1.41z' />
+        </svg>
+    );
+};
+
+export default ExpandmoreIcon;
